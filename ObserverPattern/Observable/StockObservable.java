e160c6fbc25f38373fe1d28d0631646f21903e66
yashV0201 <vardhan.yash0201@gmail.com>
2024-05-11 04:07:18 +0530

    observer [attern

---
+++ ObserverPattern/Observable/StockObservable.java
@@ -0,0 +1,11 @@
+package ObserverPattern.Observable;
+
+import ObserverPattern.Observer.*;;
+
+public interface StockObservable{
+    public void add(NotificationAlertObserver observer);
+    public void remove(NotificationAlertObserver observer);
+    public void notifySubscribers();
+    public void setStockCount(int newStockAdded);
+    public int getStockCount();
+}
